c6bd3c91b70921a853b1cce57bcfdc052a0d0ce0
zhangli <cherry.zhang@intel.com>
2017-02-08 11:10:46 +0800

    add train

--- dl/src/main/scala/com/intel/analytics/bigdl/example/test.scala
+++ dl/src/main/scala/com/intel/analytics/bigdl/example/test.scala
@@ -17,85 +17,23 @@
 
 package com.intel.analytics.bigdl.example
 
-import java.util
-
-import com.intel.analytics.bigdl.dataset.{SampleToBatch, Transformer, _}
+import com.intel.analytics.bigdl.dataset.{SampleToBatch, _}
 import com.intel.analytics.bigdl.example.MlUtils.RNNParams
 import com.intel.analytics.bigdl.nn._
 import com.intel.analytics.bigdl.numeric.NumericDouble
 import com.intel.analytics.bigdl.optim._
 import com.intel.analytics.bigdl.tensor.Tensor
-import com.intel.analytics.bigdl.utils.{Engine, T}
-import com.intel.analytics.bigdl.{DataSet => _, _}
+import com.intel.analytics.bigdl.utils.Engine
+import com.intel.analytics.bigdl.{DataSet => _}
 import org.apache.spark.SparkContext
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
-import scopt.OptionParser
 // import org.apache.spark.ml.linalg.DenseVector
 import org.apache.spark.mllib.linalg.DenseVector
 import org.apache.spark.mllib.regression.LabeledPoint
 
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
-
-object ToSample {
-  def apply(nRows: Int, nCols: Int)
-  : ToSample =
-    new ToSample(nRows, nCols)
-}
-
-class ToSample(nRows: Int, nCols: Int)
-  extends Transformer[(Array[Double], Double), Sample[Double]] {
-
-  private val buffer = new Sample[Double]()
-  private var featureBuffer: Array[Double] = null
-  private var labelBuffer: Array[Double] = null
-
-  override def apply(prev: Iterator[(Array[Double], Double)]): Iterator[Sample[Double]] = {
-
-    prev.map(x => {
-
-      if (featureBuffer == null || featureBuffer.length < nRows * nCols) {
-        featureBuffer = new Array[Double](nRows * nCols)
-      }
-      if (labelBuffer == null) {
-        labelBuffer = new Array[Double](1)
-      }
-
-      // initialize featureBuffer to 0.0
-      util.Arrays.fill(featureBuffer, 0, featureBuffer.length, 0.0f)
-
-      val length = math.min(x._1.length, nRows * nCols)
-      Array.copy(x._1, 0, featureBuffer, 0, length)
-      labelBuffer(0) = x._2
-
-      buffer.set(featureBuffer, labelBuffer,
-        Array(nRows, nCols), Array(1))
-    })
-  }
-}
-
-object MlUtils {
-  case class RNNParams(
-                            coreNumber: Int = 4,
-                            nodeNumber: Int = 1,
-                            batchSize: Int = 448
-                          )
-
-  val predictParser = new OptionParser[RNNParams]("rnn classification Example") {
-    opt[Int]('b', "batch")
-      .text("batchSize")
-      .action((x, c) => c.copy(batchSize = x))
-
-    opt[Int]('c', "core")
-      .text("cores number on each node")
-      .action((x, c) => c.copy(coreNumber = x))
-
-    opt[Int]('n', "node")
-      .text("nodes number to train the model")
-      .action((x, c) => c.copy(nodeNumber = x))
-  }
-}
 
 object Test {
   def extractor(s: String): String = {
@@ -136,48 +74,35 @@ object Test {
       // labels are Double.
       // vectors are 1000 in size array of Float
       // row example: ([0.2 0.3 -0.2 .... 0.8], 3.0)
-      val vectorizedRdd = labeled.select("label", "features").rdd.map(r =>
+      val valRDD = labeled.select("label", "features").rdd.map(r =>
         (r(1).asInstanceOf[DenseVector].toArray,
           r(0).asInstanceOf[Double] + 1.0))
-      val trainingSplit = 0.6
-      val Array(trainingRDD, valRDD) =
-        vectorizedRdd.randomSplit(Array(trainingSplit, 1 - trainingSplit))
 
       val nrows = 10
       val ncols = 100
       val batchSize = param.batchSize
-      val trainSet = DataSet.rdd(trainingRDD) -> ToSample(nrows, ncols) -> SampleToBatch(batchSize)
       val valSet = DataSet.rdd(valRDD) -> ToSample(nrows, ncols) -> SampleToBatch(batchSize)
 
-      val hiddenSize = 40
-      val bpttTruncate = 4
-      val inputSize = 100
-      val classNum = 34
+      val path_saveModel = "model_test"
+      val loaded_model = Module.load[Double](path_saveModel)
 
-      val model_N = Sequential[Double]()
-        .add(Recurrent[Double](hiddenSize, bpttTruncate)
-          .add(RnnCell[Double](inputSize, hiddenSize))
-          .add(Tanh[Double]()))
-        .add(Select(2, 10))
-        // .add(Reshape(Array(400)))
-        .add(Linear(40, classNum))
-        .add(LogSoftMax())
+      val tmpRDD = valSet.toDistributed().data(train = false)
+      val tmp1 = tmpRDD.mapPartitions(dataIter => {
+        dataIter.map(batch => {
+          val input = batch.data
+          loaded_model.forward(input).toTensor[Double]
+        })
+      }).collect()
 
-      val state = T("learningRate" -> 0.01, "learningRateDecay" -> 0.0002)
+      // tmp1.map(println(_))
 
-      val optimizer = Optimizer(
-        model = model_N,
-        dataset = trainSet,
-        criterion = new ClassNLLCriterion[Double]()
-      ).asInstanceOf[DistriOptimizer[Double]].disableCheckSingleton()
+      val validator = Validator(loaded_model, valSet)
+      val result = validator.test(Array(new Top1Accuracy[Double]))
 
-      val numEpochs = 5
-      optimizer.
-        setState(state).
-        setValidation(Trigger.everyEpoch, valSet, Array(new Top1Accuracy[Double])).
-        setOptimMethod(new Adagrad[Double]()).
-        setEndWhen(Trigger.maxEpoch(numEpochs)).
-        optimize()
+      result.foreach(r => {
+        println(s"${r._2} is ${r._1}")
+      })
+
     })
   }
 }
