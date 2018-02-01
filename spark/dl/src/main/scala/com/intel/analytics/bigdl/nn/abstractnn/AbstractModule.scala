fc294883d6f51bb1c021ee2b69ccc2cb09fc5ad1
zhangli <cherry.zhang@intel.com>
2018-02-01 16:58:10 +0800

    refacotr stream & engine

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/abstractnn/AbstractModule.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/abstractnn/AbstractModule.scala
@@ -835,7 +835,11 @@ abstract class AbstractModule[A <: Activity: ClassTag, B <: Activity: ClassTag, 
   }
 
   def setDnnEngine(loc : Long): this.type = {
-    engineLocation = loc
+    this.engineLocation = loc
+    if (this.isInstanceOf[Container[Activity, Activity, T]]) {
+      this.asInstanceOf[Container[Activity, Activity, T]].
+        modules.map(_.setDnnEngine(loc))
+    }
     this
   }
 
@@ -859,7 +863,11 @@ abstract class AbstractModule[A <: Activity: ClassTag, B <: Activity: ClassTag, 
   }
 
   def setStream(loc: Long): this.type = {
-    streamLocation = loc
+    this.streamLocation = loc
+    if (this.isInstanceOf[Container[Activity, Activity, T]]) {
+      this.asInstanceOf[Container[Activity, Activity, T]].
+        modules.map(_.setStream(loc))
+    }
     this
   }
 
