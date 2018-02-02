470160d1e886bac11cb7725cdcebcaff85fb3c64
Wang Yanzhang <i8run15@gmail.com>
2018-02-02 13:24:33 -0500

    feat: aligned tensor

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/tensor/Tensor.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/tensor/Tensor.scala
@@ -847,6 +847,8 @@ object SparseType extends TensorType
 
 object QuantizedType extends TensorType
 
+object MklDnnType extends TensorType
+
 object Tensor {
 
   // pre-load MKL library. If we do not do it here,
