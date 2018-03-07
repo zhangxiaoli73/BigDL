e914cd827c7b01fb512a8e3c453112c8ccddd7c3
dding3 <ding.ding@intel.com>
2018-03-07 08:35:26 -0800

    add polywarmup

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/models/resnet/TrainImageNet.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/models/resnet/TrainImageNet.scala
@@ -55,7 +55,7 @@ object TrainImageNet {
 
       val batchSize = param.batchSize
       val (imageSize, dataSetType, maxEpoch, dataSet) =
-        (224, DatasetType.ImageNet, param.nepochs, ImageNetDataSet)
+        (224, DatasetType.ImageNet, 90, ImageNetDataSet)
 
       val trainDataSet = dataSet.trainDataSet(param.folder + "/train", sc, imageSize, batchSize)
 
@@ -72,14 +72,14 @@ object TrainImageNet {
         if (param.optnet) {
           ResNet.shareGradInput(curModel)
         }
-//        ResNet.modelInit(curModel)
+        ResNet.modelInit(curModel)
         curModel
       }
 
       println(model)
 
       val optimMethod = if (param.stateSnapshot.isDefined) {
-        val optim = OptimMethod.load[Float](param.stateSnapshot.get).asInstanceOf[SGD[Float]]
+        val optim = OptimMethod.load[Float](param.stateSnapshot.get).asInstanceOf[LarsSGD[Float]]
         val baseLr = param.learningRate
         val iterationsPerEpoch = math.ceil(1281167 / param.batchSize).toInt
         val warmUpIteration = iterationsPerEpoch * param.warmupEpoch
@@ -101,13 +101,14 @@ object TrainImageNet {
 //          momentum = param.momentum,
 ////          larsLearningRateSchedule = SGD.EpochDecayWithWarmUp(warmUpIteration, delta, imageNetDecay),
 //          larsLearningRateSchedule =
-//          SGD.Poly(2, math.ceil(1281167.toDouble / param.batchSize).toInt * maxEpoch),
+//            SGD.PolyWithWarmUp(warmUpIteration, delta, 2, iterationsPerEpoch * 90),
 //          gwRation = 0.001
 //        )
         new SGD[Float](learningRate = param.learningRate, learningRateDecay = 0.0,
-          momentum = param.momentum, dampening = param.dampening,
-          nesterov = param.nesterov,
-          learningRateSchedule = SGD.EpochDecayWithWarmUp(warmUpIteration, delta, imageNetDecay))
+        momentum = param.momentum, dampening = param.dampening,
+        nesterov = param.nesterov,
+        learningRateSchedule = SGD.EpochDecayWithWarmUp(warmUpIteration, delta, imageNetDecay))
+//        learningRateSchedule = SGD.PolyWithWarmUp(warmUpIteration, delta, 2, iterationsPerEpoch * 90))
       }
 
       val optimizer = Optimizer(
