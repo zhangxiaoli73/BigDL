35b9a6b9961ce71aef2239acd2f0177ae69119c1
zhangli <cherry.zhang@intel.com>
2018-11-12 08:45:20 +0800

    backup

---
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/mkldnn/BigDLWrapper.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.nn.mkldnn
+
+import com.intel.analytics.bigdl.mkl.Memory
+import com.intel.analytics.bigdl.nn.abstractnn.{AbstractModule, Activity, TensorModule}
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.bigdl.utils.Shape
+import spire.syntax.module
+
+
+class BigDL2DnnWrapper(val module: AbstractModule[Tensor[_], Tensor[_], Float], formats : String)
+  extends MklDnnLayer {
+
+  output = Tensor[Float]()
+  gradInput = Tensor[Float]()
+
+  override private[mkldnn] def initFwdPrimitives(inputs: Array[MemoryData], phase: Phase) = {
+    // todo: only support tensor model and implement computeOutputShape
+    val inputShape = inputs(0).shape
+    val outputShape = module.computeOutputShape(Shape(inputs(0).shape)).toSingle().toArray
+
+    require(inputShape.length == 2 || inputShape.length == 4,
+      s"just support input shape dim is 2 or 4, but get ${inputShape.length}")
+    require(outputShape.length == 2 || outputShape.length == 4,
+      s"just support output shape dim is 2 or 4, but get ${outputShape.length}")
+
+    val realInputs = if (inputShape.length == 4) {
+      HeapData(inputShape, Memory.Format.nchw)
+    } else {
+      HeapData(inputShape, Memory.Format.nc)
+    }
+
+    val realOutputs = if (outputShape.length == 4) {
+      HeapData(outputShape, Memory.Format.nchw)
+    } else {
+      HeapData(outputShape, Memory.Format.nc)
+    }
+
+    _inputFormats = Array(realInputs)
+    _outputFormats = Array(realOutputs)
+
+    (_inputFormats, _outputFormats)
+  }
+
+  override private[mkldnn] def initBwdPrimitives(grad: Array[MemoryData], phase: Phase) = {
+    _gradOutputFormats = _outputFormats
+    _gradOutputFormatsForWeight = _outputFormats
+    _gradInputFormats = _inputFormats
+    (_outputFormats, _gradInputFormats)
+  }
+
+  override def updateOutput(input: Activity): Activity = {
+    output = module.forward(input.toTensor[Float])
+    output
+  }
+
+  override def updateGradInput(input: Activity, gradOutput: Activity): Activity = {
+    gradInput = module.updateGradInput(input.toTensor[Float], gradOutput.toTensor[Float])
+    gradInput
+  }
+
+  override def accGradParameters(input: Activity, gradOutput: Activity): Unit = {
+    module.accGradParameters(input.toTensor[Float], gradOutput.toTensor[Float])
+  }
+}
+
+
+object BigDL2DnnWrapper {
+  def apply(module: AbstractModule[Tensor[_], Tensor[_], Float], formats : String)
+  : BigDL2DnnWrapper = new BigDL2DnnWrapper(module, formats)
+}
---
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/nn/mkldnn/BigDL2DnnWrapperSpec.scala
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.nn.mkldnn
+
+import com.intel.analytics.bigdl.{Module, nn}
+import com.intel.analytics.bigdl.mkl.Memory
+import com.intel.analytics.bigdl.nn.{Squeeze, mkldnn}
+import com.intel.analytics.bigdl.nn.abstractnn.{AbstractModule, TensorModule}
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.utils.BigDLSpecHelper
+import com.intel.analytics.bigdl.utils.RandomGenerator._
+
+class BigDL2DnnWrapperSpec extends BigDLSpecHelper {
+
+  // from nhwc -> nchw
+  def shapeToNCHW(shape: Array[Int]): Array[Int] = {
+   Array(shape(0), shape(3), shape(1), shape(2))
+  }
+
+  def model(inputShape: Array[Int]) : MklDnnContainer = {
+    val model1 = mkldnn.Sequential()
+
+    model1.add(mkldnn.Input(inputShape, Memory.Format.nhwc))
+
+    val s = Squeeze[Float](Array(2, 3), true).
+      asInstanceOf[AbstractModule[Tensor[_], Tensor[_], Float]]
+
+    model1.add(BigDL2DnnWrapper(s, ""))
+
+    model1
+  }
+
+  def wrapConv(inputShape: Array[Int], gradOutShape : Array[Int]) : MklDnnContainer = {
+    import com.intel.analytics.bigdl.nn
+    RNG.setSeed(100)
+
+    val model1 = mkldnn.Sequential()
+
+    model1.add(mkldnn.Input(inputShape, Memory.Format.nhwc).setName("input"))
+
+    val outputShape = shapeToNCHW(inputShape)
+
+    model1.add(ReorderMemory(inputFormat = HeapData(inputShape, Memory.Format.nhwc),
+      outputFormat = HeapData(outputShape, Memory.Format.nchw),
+      gradInputFormat = null, gradOutputFomat = null))
+
+    // conv with NCHW
+    val s = nn.SpatialConvolution[Float](3, 32, 5, 5, 1, 1).
+      asInstanceOf[AbstractModule[Tensor[_], Tensor[_], Float]]
+//    s.getParameters()._1.fill(0.0001f)
+//    s.getParameters()._2.fill(0.0001f)
+
+//    val p = s.getParametersTable()
+//    p.get[Tensor[Float]]("weight").get.copy(initWeight)
+//    p.get[Tensor[Float]]("bias").get.copy(initBias)
+
+
+    model1.add(BigDL2DnnWrapper(s, "").setName("wrapper"))
+
+//    model1.add(ReorderMemory(
+//      inputFormat = null,
+//      outputFormat = null,
+//      gradInputFormat = null,
+//      gradOutputFomat = HeapData(gradOutShape, Memory.Format.nhwc)).setName("test"))
+    model1
+  }
+
+   "wrapper squeeze" should "be correct" in {
+     val in = Tensor[Float](2, 1, 1, 3).rand()
+     val inNHWC = in.transpose(2, 4).transpose(3, 4).contiguous().clone()
+
+     val s = Squeeze[Float](Array(2, 3), true).
+       asInstanceOf[AbstractModule[Tensor[_], Tensor[_], Float]]
+
+     val w = model(inNHWC.size()) // BigDL2DnnWrapper(s, "")
+
+     w.compile(Phase.InferencePhase)
+
+     val out = w.forward(inNHWC)
+
+     val tm = 0
+   }
+
+  "wrapper conv" should "be correct" in {
+    val nIn = 3
+    val nOut = 32
+    val kH = 5
+    val kW = 5
+    val in = Tensor[Float](4, 3, 7, 7).rand()
+    val inNHWC = in.transpose(2, 3).transpose(3, 4).contiguous().clone()
+
+    val gradOutput = Tensor[Float](4, 32, 3, 3).rand()
+    val gradOutputNHWC = gradOutput.transpose(2, 3).transpose(3, 4).contiguous().clone()
+
+
+    RNG.setSeed(100)
+    val model = nn.SpatialConvolution[Float](3, 32, 5, 5, 1, 1).
+      asInstanceOf[AbstractModule[Tensor[_], Tensor[_], Float]]
+
+
+    // wrapper
+    val wrapperModel = wrapConv(inNHWC.size(), gradOutputNHWC.size())
+    wrapperModel.compile(Phase.InferencePhase)
+
+    val wrapperOut = wrapperModel.forward(inNHWC)
+    val out = model.forward(in) // size 4, 32, 3, 3
+
+    // wrapperOut.equals(out) should be(true)
+
+    // for backward
+    val wrapperGrad = wrapperModel.backward(inNHWC, gradOutputNHWC)
+    val grad = model.backward(in, gradOutput)
+    val gradNHWC = grad.transpose(2, 3).transpose(3, 4).contiguous().clone()
+
+    println("done")
+  }
+}
