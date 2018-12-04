bbdd8d125e62616f1b548fac0d049a1a3d5a58e9
zhangli <380761639@qq.com>
2018-12-04 14:58:18 +0800

    test perf

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/utils/mkldnn/DistriPerf.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/utils/mkldnn/DistriPerf.scala
@@ -17,6 +17,7 @@
 package com.intel.analytics.bigdl.utils.mkldnn
 
 import com.intel.analytics.bigdl.Module
+import com.intel.analytics.bigdl.dataset.{MiniBatch, Sample}
 import com.intel.analytics.bigdl.example.loadmodel.AlexNet
 import com.intel.analytics.bigdl.mkl.Memory
 import com.intel.analytics.bigdl.models.inception.Inception_v1_NoAuxClassifier
@@ -28,11 +29,13 @@ import com.intel.analytics.bigdl.nn.Graph._
 import com.intel.analytics.bigdl.nn._
 import com.intel.analytics.bigdl.nn.abstractnn.Activity
 import com.intel.analytics.bigdl.numeric.NumericFloat
+import com.intel.analytics.bigdl.optim.Top1Accuracy
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.bigdl.utils.RandomGenerator._
 import com.intel.analytics.bigdl.utils.{Engine, T, Table}
 import org.apache.log4j.Logger
+import org.apache.spark.SparkContext
 import scopt.OptionParser
 
 import scala.collection.mutable.ArrayBuffer
@@ -55,38 +58,25 @@ object Perf {
     opt[Int]('i', "iteration")
       .text("Iteration of perf test. The result will be average of each iteration time cost")
       .action((v, p) => p.copy(iteration = v))
-    opt[Boolean]('t', "training")
-      .text(s"Perf test training or testing")
-      .action((v, p) => p.copy(training = v))
   }
 
   def main(argv: Array[String]): Unit = {
     parser.parse(argv, new ResNet50PerfParams()).foreach { params =>
-      System.setProperty("bigdl.localMode", "true")
       System.setProperty("bigdl.engineType", "mkldnn")
+      val conf = Engine.createSparkConf().setAppName("Test perf for convertion")
+        .set("spark.akka.frameSize", 64.toString)
+        .set("spark.task.maxFailures", "1")
+      val sc = new SparkContext(conf)
       Engine.init
 
       val batchSize = params.batchSize
-      val training = params.training
       val iterations = params.iteration
 
       val inputFormat = Memory.Format.nchw
       var input : Tensor[Float] = null
       var label : Tensor[Float] = null
 
-      val p = // "/home/zhangli/workspace/zoo-model/analytics-zoo_frcnn-pvanet-compress_PASCAL_0.1.0.model"
- //     "/home/zhangli/workspace/zoo-model/analytics-zoo_frcnn-pvanet_PASCAL_0.1.0.model"
-//      "/home/zhangli/workspace/zoo-model/analytics-zoo_frcnn-vgg16-compress_PASCAL_0.1.0.model"
-//      "/home/zhangli/workspace/zoo-model/analytics-zoo_frcnn-vgg16_PASCAL_0.1.0.model"
-//      "/home/zhangli/workspace/zoo-model/analytics-zoo_inception-v3_imagenet_0.1.0.model"
-//      "/home/zhangli/workspace/zoo-model/analytics-zoo_squeezenet_imagenet_0.1.0.model"
-//      "/home/zhangli/workspace/zoo-model/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model"
-      "/home/zhangli/workspace/zoo-model/analytics-zoo_ssd-vgg16-300x300_PASCAL_0.1.0.model"
-//      "/home/zhangli/workspace/zoo-model/analytics-zoo_ssd-vgg16-512x512_PASCAL_0.1.0.model"
-//      "/home/zhangli/workspace/zoo-model/analytics-zoo_vgg-19_imagenet_0.1.0.model"
-
       val modelLoad = Module.loadModule[Float](params.modelPath)
-
       val graph = if (!modelLoad.isInstanceOf[Graph[Float]]) modelLoad.toGraph() else modelLoad
       val model = graph.asInstanceOf[StaticGraph[Float]].toIRgraph(5, Memory.Format.nc)
       model.build()
@@ -103,35 +93,20 @@ object Perf {
         case _ => throw new UnsupportedOperationException(s"Unkown model ${params.dataType}")
       }
 
-      val criterion = CrossEntropyCriterion()
+      model.evaluate()
+      val broadcast = sc.broadcast(input, label)
+      val length = batchSize * Engine.nodeNumber() * Engine.coreNumber() * iterations
+      val rdd = sc.parallelize((1 to length), Engine.nodeNumber)
+        .mapPartitions(iter => {
+          val broad = broadcast.value
+          Iterator.single(Sample(broad._1.clone().rand(), broad._2.clone().rand()))
+        }).persist()
 
-      if (training) {
-        model.training()
-      } else {
-        model.evaluate()
-      }
-
-      var iteration = 0
       val start = System.nanoTime()
-      val throughputs = new ArrayBuffer[Float]()
-      while (iteration < iterations) {
-        val start = System.nanoTime()
-        val output = model.forward(input)
-
-        if (training) {
-          val _loss = criterion.forward(output, label)
-          val errors = criterion.backward(output, label).toTensor
-          model.backward(input, errors)
-        }
-
-        val takes = System.nanoTime() - start
-        throughputs.append(batchSize.toFloat / (takes / 1e9).toFloat)
-        val throughput = "%.2f".format(batchSize.toFloat / (takes / 1e9))
-        logger.info(s"Iteration $iteration, takes $takes s, throughput is $throughput imgs/sec")
-
-        iteration += 1
-      }
-      val avg = throughputs.toArray.reduce((a, b) => (a + b)) / iterations
+      val result = model.evaluate(rdd,
+        Array(new Top1Accuracy[Float]), Some(batchSize * Engine.nodeNumber() * Engine.coreNumber()))
+      val takes = System.nanoTime() - start
+      val avg = takes / iterations / 1e9
       logger.info(s"Average throughput is $avg imgs/sec")
     }
   }
@@ -139,8 +114,7 @@ object Perf {
 
 case class ResNet50PerfParams (
   batchSize: Int = 4,
-  iteration: Int = 80,
-  training: Boolean = false,
+  iteration: Int = 50,
   dataType: String = "ssd",
   modelPath: String = "imagenet"
 )
